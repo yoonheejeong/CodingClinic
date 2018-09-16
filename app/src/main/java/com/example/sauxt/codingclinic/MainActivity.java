190016685a330d66ea04c2f59f0488d8aa656470
yoonheejeong <sauxt66@hanmail.net>
2018-09-16 20:07:41 +0900

    HW1 finished!

--- app/src/main/java/com/example/sauxt/codingclinic/MainActivity.java
+++ app/src/main/java/com/example/sauxt/codingclinic/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.sauxt.codingclinic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean toggle = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button btn = (Button)findViewById(R.id.myButton);
+        final TextView mainText = (TextView) findViewById(R.id.mainText);
+
+        btn.setOnClickListener(new Button.OnClickListener(){
+            public void onClick(View view){
+                if(toggle){
+                    mainText.setText("What is next?");
+                }else{
+                    mainText.setText("First Step");
+                }
+
+                toggle = !toggle;
+
+            }
+        });
+
     }
 }
